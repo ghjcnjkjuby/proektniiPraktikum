f63162035ad8c6ef5e80a6c0c90e90fde88fc8b8
Alexander <50043591+ghjcnjkjuby@users.noreply.github.com>
2020-01-21 19:44:52 +0300

    Add files via upload

---
+++ lab5.py
@@ -0,0 +1,19 @@
+import os
+print("Стандартная директория: " + str(os.getcwd()))
+print("Имя: " + str(os.getlogin()))
+while(True):
+    try:
+        dir = input("Укажите путь к директории:")
+        print("Выбранная директория:" + dir)
+        print("Файлы/папки в данной директории:")
+        print(os.listdir(dir))
+        nextOperation = input("Для просмотра другой директории напишите: другая\nДля выхода напишите: выход\n")
+        if(nextOperation == "выход"):
+            break
+        elif(nextOperation == "другая"):
+            pass
+        elif(nextOperation == nextOperation):
+            break
+    except (FileNotFoundError):
+        print("Такой директории нет!")
+© 2020 GitHub, Inc.
