a8fa8ef58defc6c6dc88232feebc94b6b64cfdf3
Alexander <50043591+ghjcnjkjuby@users.noreply.github.com>
2020-01-21 19:17:12 +0300

    Add files via upload

---
+++ lab6.py
@@ -0,0 +1,29 @@
+import shutil
+bool = True
+while(True):
+    while(bool == True):
+        startOperation = input("Напишите старт/start чтобы начать операцию!\n")
+        if(startOperation == "старт" or startOperation == "start"):
+            bool = False
+        else:
+            print("Ошибка в стартовом слове!")
+    first = input("Путь файла,который надо скопировать:")
+    second = input("Путь куда копировать:")
+    try:
+        if(startOperation == "старт" or startOperation == "start"):
+            shutil.copy(first, second)
+            bool = True
+        else:
+            print("Ошибка в стартовом слове!")
+            bool = True
+    except (FileNotFoundError):
+        print("Неправильно указан путь!")
+        bool = True
+    nextOperation = input("Для просмотра продолжения напишите: продолжить\nДля выхода напишите: выход\n")
+    if(nextOperation == "выход"):
+        break
+    elif(nextOperation == "другая"):
+        pass
+    elif(nextOperation == nextOperation):
+        break
+    
